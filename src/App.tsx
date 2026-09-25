b72814206325856f3a643df8483f4a01534d09fc
Simão Pinto <simaopinto06@ua.pt>
2026-09-25 18:17:00 +0100

    Prepare GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ import { Team } from './pages/Team'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
-  useEffect(() => window.scrollTo(0, 0), [pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
   return null
 }
 
